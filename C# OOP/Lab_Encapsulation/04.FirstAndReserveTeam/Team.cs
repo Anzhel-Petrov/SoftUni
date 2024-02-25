6e8bb480f521053b17ea8333f08c16bb771810b5
Anzhel Petrov <anzhel_petrov@hotmail.com>
2024-02-25 10:39:35 +0200

    Rest of the exercises

---
+++ C# OOP/Lab_Encapsulation/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private List<Person> _firstTeam;
+        private List<Person> _reserveTeam;
+        private string _name;
+
+        public Team(string name)
+        {
+            _name = name;
+            _firstTeam = new List<Person>();
+            _reserveTeam = new List<Person>();
+            
+        }
+
+        public string Name { get; set; }
+        public IReadOnlyCollection<Person> FirstTeam { get => _firstTeam.AsReadOnly(); }
+        public IReadOnlyCollection<Person> ReserveTeam { get => _reserveTeam.AsReadOnly(); }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                _firstTeam.Add(person);
+            }
+            else
+            {
+                _reserveTeam.Add(person);
+            }
+        }
+    }
+}
