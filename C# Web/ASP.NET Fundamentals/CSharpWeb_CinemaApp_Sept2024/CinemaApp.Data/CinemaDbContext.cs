86b98edaa6dfaf0b54f99e6b9fd687d8d351ecc3
Anzhel-Petrov <anzhel_petrov@hotmail.com>
2025-01-03 18:36:54 +0200

    Add EF and DbContext

---
+++ C# Web/ASP.NET Fundamentals/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Data/CinemaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CinemaApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext()
+        {
+            
+        }
+
+        public CinemaDbContext(DbContextOptions options) 
+            : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
---
+++ C# Web/ASP.NET Fundamentals/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Data/Configurations/MovieConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CinemaApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static CinemaApp.Common.EntityValidationConstants.Movie;
+
+namespace CinemaApp.Data.Configurations;
+
+public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.HasKey(m => m.Id);
+
+        builder
+            .Property(m => m.Title)
+            .IsRequired()
+            .HasMaxLength(TitleMaxLength);
+
+        builder
+            .Property(m => m.Genre)
+            .IsRequired()
+            .HasMaxLength(GenreMaxLength);
+    }
+}
