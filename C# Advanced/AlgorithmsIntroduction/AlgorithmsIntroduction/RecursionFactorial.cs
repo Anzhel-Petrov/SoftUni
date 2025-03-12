3b979302ad0b50536051a93e857f0a69129e9375
Anzhel-Petrov <anzhel_petrov@hotmail.com>
2025-03-12 09:01:19 +0200

    Revisiting C# Advanced recursion and algorithms

---
+++ C# Advanced/AlgorithmsIntroduction/AlgorithmsIntroduction/RecursionFactorial.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlgorithmsIntroduction
+{
+    internal class RecursionFactorial
+    {
+        static void Main(string[] args)
+        {
+            int FactorialRecursive(int n)
+            {
+                //return n <= 1 ? 1 : n * FactorialRecursive(n - 1);
+                if (n == 1)
+                {
+                    return 1;
+                }
+
+                int result = n * FactorialRecursive(n - 1);
+
+                return result;
+            }
+
+            Console.WriteLine(FactorialRecursive(6));
+        }
+    }
+}
