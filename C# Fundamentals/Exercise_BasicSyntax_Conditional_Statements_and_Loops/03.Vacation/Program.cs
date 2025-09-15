7b7ada4b2a95b6b9908227d5592f324d580c0c10
Anzhel-Petrov <anzhel_petrov@hotmail.com>
2025-09-15 12:30:57 +0300

    Going back to basics

---
+++ C# Fundamentals/Exercise_BasicSyntax_Conditional_Statements_and_Loops/03.Vacation/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var peopleCount = int.Parse(Console.ReadLine());
+            var groupType = Console.ReadLine();
+            var weekDay = Console.ReadLine();
+            double pricePerPerson = 0;
+
+            switch (groupType)
+            {
+                case "Students":
+                    if (weekDay == "Friday")
+                    {
+                        pricePerPerson = 8.45;
+                    }
+                    if (weekDay == "Saturday")
+                    {
+                        pricePerPerson = 9.8;
+                    }
+                    if (weekDay == "Sunday")
+                    {
+                        pricePerPerson = 10.46;
+                    }
+                    break;
+
+                case "Business":
+                    if (weekDay == "Friday")
+                    {
+                        pricePerPerson = 10.9;
+                    }
+                    if (weekDay == "Saturday")
+                    {
+                        pricePerPerson = 15.6;
+                    }
+                    if (weekDay == "Sunday")
+                    {
+                        pricePerPerson = 16;
+                    }
+                    break;
+
+                case "Regular":
+                    if (weekDay == "Friday")
+                    {
+                        pricePerPerson = 15;
+                    }
+                    if (weekDay == "Saturday")
+                    {
+                        pricePerPerson = 20;
+                    }
+                    if (weekDay == "Sunday")
+                    {
+                        pricePerPerson = 22.5;
+                    }
+                    break;
+            }
+
+            double totalPrice = pricePerPerson * peopleCount;
+
+            if (groupType == "Students" && peopleCount >= 30)
+            {
+                totalPrice = totalPrice * 0.85;
+            }
+            else if (groupType == "Business" && peopleCount >= 100)
+            {
+                totalPrice = pricePerPerson * (peopleCount - 10);
+            }
+            else if (groupType == "Regular" && peopleCount >= 10 && peopleCount <= 10)
+            {
+                totalPrice = totalPrice * 0.95;
+            }
+
+            Console.WriteLine($"Total price: {totalPrice:F2}");
+        }
+    }
+}
---
+++ C# Fundamentals/Exercise_BasicSyntax_Conditional_Statements_and_Loops/05.Login/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = Console.ReadLine();
+            var attemots = 0;
+            var reversePassword = "";
+
+            for (int i = password.Length - 1; i >= 0; i--)
+            {
+                reversePassword += password[i];
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                attemots++;
+
+                if (input == reversePassword)
+                {
+                    Console.WriteLine($"User {password} logged in.");
+                    break;
+                }
+                else if (attemots == 4)
+                {
+                    Console.WriteLine($"User {password} blocked!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+        }
+    }
+}
