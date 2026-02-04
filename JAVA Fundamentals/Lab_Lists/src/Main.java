eff5c2a3b9631d82013d645aa19704f32664dbb5
Anzhel-Petrov <anzhel_petrov@hotmail.com>
2026-02-04 12:38:26 +0200

    JAVA Fundamentals

---
+++ JAVA Fundamentals/Lab_Lists/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //List<String> list1 = new ArrayList<>();
+        var list2 = List.of(1,2,3,4,5,6,7,8);
+        var foor = list2.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 10)
+                .toList();
+
+        System.out.println(foor);
+        foor.forEach(System.out::println);
+    }
+}
